9501daaf08e9fdd1e64e9b4d6bdc82f723c68e18
Gianpaolo <pao.barga@gmail.com>
2021-01-12 18:46:22 +0100

    esercizio bonus finito

--- bonus/script-bonus.js
+++ bonus/script-bonus.js
@@ -16,3 +16,30 @@ $("div p").click(function() {
         this.style.color = "lightblue";
     }
 })
+
+//chaining ad dbclick on button to add a class smw else
+$("button")
+    .mouseenter(function() {
+        $(this).addClass("highlight");
+    })
+    .mouseleave(function() {
+        $(this).removeClass("highlight");
+    });
+
+$("button:first-child()").click(function() {
+// !!!! non ho potuto usare !==, altrimenti nn riconosce la condizione 0
+    if(document.querySelector("h1").style.opacity != 0) {
+        $("h1").fadeTo(250, 0);
+    } else {
+        $("h1").fadeTo(250, 1);
+    }
+})
+
+$("button:nth-child(2)")
+    .dblclick(function() {
+        $(".target:nth-child(2)").hide();
+        setTimeout(
+            function() {
+                $(".target:nth-child(2)").show()
+            }, 540)
+    });
